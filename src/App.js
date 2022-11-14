5375f7c80cbc7786268f9fc51bf9b09442d8cc94
Raul:Chang <4toulquiorraschiffer@gmail.com>
2022-11-14 18:38:40 -0300

    Entrega

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/NavBar';
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartContextProvider } from "./storage/CartContext";
